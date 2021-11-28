b924d4bba32ce33dd409d0f58faf247d94b48f50
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-28 15:32:11 -0500

    commit tree 2217270712d2410e1bd4be5d713c5dd05e3ace02

--- index.js
+++ index.js
@@ -12,5 +12,32 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(splitted) {
+    return splitted.split(' ').map(function(capitalizeFirstLetter) {
+      return capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
+    }).join(' ')
+  })
 }
+console.log(titleCased(tutorials));
+
+//function titleCased(array) {
+  //var wordArr = array.split(" ");
+  //var newArr = wordArr.map(function(word) {
+    //return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() ;
+  //});
+  //return newArr.join(' ');
+//}
+
+//console.log(titleCased(tutorials));
+
+//= newTu.map(function(item) {
+//return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+//})
+//return titleCased;
+//}
+
+
+  //console.log(titleCased(tutorials));
+  //.split(" ")
+  //.map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+  //.join(" ")
